fa63f64a73528fc725fb6cac31d2550980014bbc
umimametimame <22ci0432@jec.ac.jp>
2023-12-08 01:12:59 +0900

    Did: ・海と地面の追加

---
+++ Assets/Script/GameScene_Operator.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScene_Operator : SceneOperator
+{
+    [SerializeField] private GravityProfile scrollGravity;
+}
